9f0b48e9e4f7287f2759e9c5b14a64d08b8c47bf
febinphilips <158031033+febinphilips@users.noreply.github.com>
2024-01-29 15:02:57 +0530

    Update Add2numbers.c
    febs changes

--- Add2numbers.c
+++ Add2numbers.c
@@ -4,9 +4,8 @@ int main() {
     int number1, number2, sum;
     
     printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
-
-    // calculate the sum
+    scanf("%d %d", &number1, &number2)
+        
     sum = number1 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
